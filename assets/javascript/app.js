9de18cf755c58809de6fe0a2083e35bfa15c576e
dlopez079 <dlopez079@gmail.com>
2019-09-10 15:43:45 -0400

    Removed content from the javascript to start all over.  Commented out and in the link to the reset.css to show colleage that it is not allowing me to changed the text on index.html.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,33 +4,19 @@ $(document).ready(function() {
     //Insert Total Trivial Trivia in #banner.
     $("#banner").html("<h1>Total Trivial Trivia!</h1>");
 
-    //establish variabled needed for the javascript
-    //Set the time we're counting down to
-    var countDownTime = new time("11:13:25").getTime();
+    $("#main").html("Question </br> Answer");
 
-    //Update the counte down every 1 second
-    var x = setInterval(function() {
-
-    //Get today's time
-    var now = getTime();
-
-    //Find the distance between now and the count down date
-    var countDownTime - now;
-
-    //time calculations for hours, minutes and seconds
-    var hours = Math.floor((distance %(1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-//Output the result in the #main with id.
-    $("#main").html = hours + minutes + seconds");
-
-S
+    //establish global variables
+    var card = $("#main");
+    var countStartNumber = 30;
+    var timer;
+})
 
 
-    //Insert body content onto HTML page. 
-    $("#main").html("Time Remaining: " + timeRemaining);
+    
+
+    
+
+    
 
 
-
-}); //END OF DOCUMENT READY!!!!
