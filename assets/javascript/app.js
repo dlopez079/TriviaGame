4caac7289e8808dfac1ed70affb4b5fe3b4ab16b
dlopez079 <dlopez079@gmail.com>
2019-09-09 23:00:55 -0400

    Made changes to the ccs for title and background color.  Commented out reset.css link from index.html because it was not allowing me to style the html document.  Linked Javascript file and confirmed that it is linked.

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    console.log("JS Linked")
+    
+    //Insert Total Trivial Trivia in #banner.
+    $("#banner").html("<h1>Total Trivial Trivia!</h1>");
+
+}); //END OF DOCUMENT READY!!!!
