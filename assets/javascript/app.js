8cd4e7cad7889bfac3a42c353365ad08c2dbcfc1
dlopez079 <dlopez079@gmail.com>
2019-09-10 18:05:50 -0400

    I structed the app.js document.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,12 +4,64 @@ $(document).ready(function() {
     //Insert Total Trivial Trivia in #banner.
     $("#banner").html("<h1>Total Trivial Trivia!</h1>");
 
-    $("#main").html("Question </br> Answer");
-
     //establish global variables
     var card = $("#main");
     var countStartNumber = 30;
     var timer;
+
+    var questions = [
+        {
+        question: "Who was the legendary Benedictine monk who invented champagne?",
+        answers: ["1","2","3","4"],
+        correctAnswers: "",
+        }   
+         
+    ]
+
+    //Create a game object that will hold all the logic of the game.
+    var game = { 
+        question: questions,
+        count: countStartNumber,
+        currentQuestion: 0,
+        correct: 0,
+        incorrect: 0,
+
+        countdown: function (){
+            //drecement counter
+            //display counter on a page
+            //run timeUp
+        },
+
+        loadQuestion: function(){
+
+        },
+        nextQuestion: function(){
+
+        },
+        timeUp: function(){
+
+        },
+        results: function(){
+
+        },
+        clicked: function(){
+
+        }, 
+        answerInCorrectly: function () {
+            
+        },
+        answerInCorrectly: function(){
+
+        },
+        reset: function(){
+
+        }
+    }
+
+    $(document).on("click", )  //that calls to reset the game function
+    $(document).on("click", )  //that calls the clicked function
+    $(document).on("click", )  //that calls the loadQuestion function.
+
 })
 
 
